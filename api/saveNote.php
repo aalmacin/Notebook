334ebb7c39c58003b5c6b4c0c83b5548a1c3100c
Aldrin Jerome Almacin <aldrin.almacin@babyrobot.com>
2014-03-16 14:04:00 -0400

    Added insert table

--- api/saveNote.php
+++ api/saveNote.php
@@ -1,9 +1,23 @@
 <?php
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASSWORD', 'root');
+  define('DB_NAME', 'devchallenge');
+
   $note = $_REQUEST['note'];
   $id = $_REQUEST['id'];
+  $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+  $note = $conn->real_escape_string($note);
   if (!empty($id)) {
-    $sql = "UPDATE Note SET note='$note' WHERE ID=$id";
+    $sql = "UPDATE Notes SET note='$note' WHERE ID=$id";
   } else {
-    $sql = "INSERT INTO Note(`note`) VALUES ('$note')";
+    $sql = "INSERT INTO Notes(`note`, `userID`) VALUES ('$note', 1)";
   }
+  if($conn->query($sql) === false) {
+    trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+  } else {
+    $last_inserted_id = $conn->insert_id;
+    $affected_rows = $conn->affected_rows;
+  }
+  $conn->close();
 ?>
