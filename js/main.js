9f684fb9403b931e37fa1f802a8c756c5e1033d8
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2014-03-16 13:16:33 -0400

    Added new note

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   $('#addNoteButton .addNote').click(function() {
     $("#noteOverlay").show();
   });
+
+  $('#noteOverlay .cancelButton').click(function() {
+    $("#noteOverlay").hide();
+    $("#noteOverlay .newNote").val("");
+  });
 });
