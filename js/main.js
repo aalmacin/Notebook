bf81615804cf1c44a9b36b0ecd327fe3935a9f55
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2014-03-16 13:22:21 -0400

    Added text in textarea val

--- js/main.js
+++ js/main.js
@@ -7,4 +7,10 @@ $(document).ready(function() {
     $("#noteOverlay").hide();
     $("#noteOverlay .newNote").val("");
   });
+
+  $('.note').click(function() {
+    $("#noteOverlay").show();
+    var txt = $($(this).find("span.noteTxt")).text();
+    $("#noteOverlay .newNote").val(txt);
+  });
 });
